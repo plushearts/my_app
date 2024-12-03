e89c387c9724abe6f01c9df4f9259a23d8e7f355
plushearts <58967537+plushearts@users.noreply.github.com>
2024-12-03 11:00:24 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,5 +1,14 @@
 import streamlit as st
-import pandas as pd
+
 
 def app():
-  st.subheader('HI')
+    st.title('MY APP')
+    st.markdown('———————————————————————')
+    st.markdown('*日常数据表，最近维护时间*：**2024-12-03**')
+    st.markdown('*登录数据表，最近维护时间*：**2024-12-01**')
+    st.markdown('*企微数据表，最近维护时间*：**2024-11-27**')
+    st.markdown('========================================')
+
+
+if __name__ == '__main__':
+    app()
