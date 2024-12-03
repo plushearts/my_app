cd5f236ed2dfe329d90d3aecc7c19f25a1316754
plushearts <58967537+plushearts@users.noreply.github.com>
2024-12-03 11:10:16 +0800

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -9,6 +9,14 @@ def app():
     st.markdown('*企微数据表，最近维护时间*：**2024-11-27**')
     st.markdown('========================================')
 
+def example():
+    if st.checkbox("Use url", value=True):
+        add_logo("http://placekitten.com/120/120")
+    else:
+        add_logo("gallery/kitty.jpeg", height=300)
+    st.write("👈 Check out the cat in the nav-bar!")
+
 
 if __name__ == '__main__':
     app()
+    example()
